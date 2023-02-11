1000340d780abf4982fee6721f0d3754e08dbf95
Christian <christianalvarez11@gmail.com>
2023-02-11 13:08:41 +0100

    changed api key

--- server/server.js
+++ server/server.js
@@ -47,5 +47,5 @@ app.post('/', async (req, res) => {
 });
 
 app.listen(5000, () =>
-  console.log('server is running on port http://localhost:5000')
+  console.log('server is running on port https://codex-burz.onrender.com')
 );
